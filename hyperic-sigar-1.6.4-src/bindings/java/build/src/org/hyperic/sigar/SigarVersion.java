96c0f772b2c5efa5b9bbf0ed5104ebeca3cf1df7
SergioMendes93 <smendes_93@hotmail.com>
2017-03-26 13:37:49 +0100

    first commit

---
+++ hyperic-sigar-1.6.4-src/bindings/java/build/src/org/hyperic/sigar/SigarVersion.java
@@ -0,0 +1,10 @@
+package org.hyperic.sigar;
+
+class SigarVersion {
+
+    static final String BUILD_DATE = "03/25/2017 01:58 PM";
+
+    static final String SCM_REVISION = "exported";
+
+    static final String VERSION_STRING = "1.6.4.0";
+}
